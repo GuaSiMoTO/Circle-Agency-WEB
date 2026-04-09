db6de998f4e02c5ba52270e56293ec5c58037eb3
GuaSiMoTO <gemelogemelo@hotmail.com>
2026-04-09 17:10:05 +0200

    Botón arrow terminado

--- Scripts/index.js
+++ Scripts/index.js
@@ -20,3 +20,29 @@ window.addEventListener('click', (e) => {
         mobileMenu.classList.remove('active');
     }
 });
+
+// Botón "Volver al inicio"
+// Seleccionamos el botón por su ID
+const btnBackToTop = document.getElementById("btn-back-to-top");
+
+// Función para mostrar/ocultar el botón según el scroll
+window.onscroll = function() {
+    scrollFunction();
+};
+
+function scrollFunction() {
+    // Si el usuario baja más de 400px desde el tope, aparece el botón
+    if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+        btnBackToTop.style.display = "flex"; // Cambiamos de none a flex para mostrar el botón
+    } else {
+        btnBackToTop.style.display = "none";
+    }
+}
+
+// Función para que al hacer clic suba suavemente
+btnBackToTop.addEventListener("click", function() {
+    window.scrollTo({
+        top: 0, // manda al tope de la página
+        behavior: "smooth" // Esto hace que el deslizamiento hacia arriba sea fluido
+    });
+});
